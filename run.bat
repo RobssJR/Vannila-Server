e151c3f7af0c23fa2acde333e82520a2fc4b0d39
RobssJR <robson.alex.jr@gmail.com>
2023-06-28 23:06:23 -0300

    Update

--- run.bat
+++ run.bat
@@ -1,13 +1,10 @@
-rem Comando para atualizar os arquivos do repositório
 git pull
 
 rem Comando para executar o paper.jar
 set command=java -Xms6G -Xmx6G -jar paper.jar --nogui
 
-rem Executa o comando em segundo plano
 start "" %command%
 
-rem Aguarda o processo do paper.jar ser encerrado
 :waitloop
 timeout /t 2 /nobreak >nul
 tasklist | find /i "java.exe" >nul
@@ -15,8 +12,7 @@ if errorlevel 1 goto process_ended
 goto waitloop
 
 :process_ended
-rem Comando para atualizar o repositório novamente
 git pull
 git add .
-git commit -m "Atualização após a execução do paper.jar"
+git commit -m Update"
 git push
